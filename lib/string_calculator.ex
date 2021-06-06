448f2fec2a25381729af0f471e194b1d783f27f3
Kevin Poston <kevposton@gmail.com>
2021-06-05 21:29:34 -0400

    add ! since it can raise exception

--- lib/string_calculator.ex
+++ lib/string_calculator.ex
@@ -1,8 +1,8 @@
 defmodule StringCalculator do
-  def add(""), do: 0
-  def add("//" <> rest), do: get_custom_delimiters(rest) |> add
-  def add(values) when is_bitstring(values), do: get_default_delimiters(values) |> add
-  def add({values, delimiters}) do
+  def add!(""), do: 0
+  def add!("//" <> rest), do: get_custom_delimiters(rest) |> add!
+  def add!(values) when is_bitstring(values), do: get_default_delimiters(values) |> add!
+  def add!({values, delimiters}) do
     numbers = String.split(values, delimiters) |> Enum.map(&(String.to_integer(&1)))
 
     case Enum.any?(numbers, &(&1 < 0)) do
--- test/string_calculator_test.exs
+++ test/string_calculator_test.exs
@@ -3,46 +3,46 @@ defmodule StringCalculatorTest do
   doctest StringCalculator
 
   test "empty string returns 0" do
-    assert StringCalculator.add("") == 0
+    assert StringCalculator.add!("") == 0
   end
 
   test "given 1 returns 1" do
-    assert StringCalculator.add("1") == 1
+    assert StringCalculator.add!("1") == 1
   end
 
   test "1,2 returns 3" do
-    assert StringCalculator.add("1,2") == 3
+    assert StringCalculator.add!("1,2") == 3
   end
 
   test "multiple numbers are handled" do
-    assert StringCalculator.add("3,3,3") == 9
+    assert StringCalculator.add!("3,3,3") == 9
   end
 
   test "new lines can be used" do
-    assert StringCalculator.add("3\n3,3") == 9
+    assert StringCalculator.add!("3\n3,3") == 9
   end
 
   test "accepts custom delimeters" do
-    assert StringCalculator.add("//;\n1;2") == 3
+    assert StringCalculator.add!("//;\n1;2") == 3
   end
 
   assert_raise RuntimeError, ~r/^Negatives not allowed/, fn ->
-    StringCalculator.add("1,-2")
+    StringCalculator.add!("1,-2")
   end
 
   test "numbers larger than 1000 are ignored" do
-    assert StringCalculator.add("1001,2") == 2
+    assert StringCalculator.add!("1001,2") == 2
   end
 
   test "delimiters can be any length" do
-    assert StringCalculator.add("//[***]\n1***2***3") == 6
+    assert StringCalculator.add!("//[***]\n1***2***3") == 6
   end
 
   test "multiple custom delimiters" do
-    assert StringCalculator.add("//[*][%]\n1*2%3") == 6
+    assert StringCalculator.add!("//[*][%]\n1*2%3") == 6
   end
 
   test "multiple custom delimiters with various length" do
-    assert StringCalculator.add("//[***][%]\n1***2%3") == 6
+    assert StringCalculator.add!("//[***][%]\n1***2%3") == 6
   end
 end
